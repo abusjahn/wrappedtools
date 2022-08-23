a2046f49a4ababd9c2ed28ccc64ab18644c32d9f
BilalsGituation <billyasser@hotmail.co.uk>
2022-08-23 11:47:23 +0200

    merge fix

---
+++ tests/testthat/test-meansd.R
@@ -0,0 +1,12 @@
+
+# out1 <- meansd(x = mtcars$mpg)
+# out2 <- meansd(x = mtcars$mpg, roundDig = 5, groupvar = mtcars$cyl, 
+#                range = TRUE, add_n = TRUE, .german = TRUE)
+# saveRDS(list(out1=out1, out2=out2),file = 'tests/testthat/meansdout.rda')
+test_that("meansd() with defaults and options set", {
+  expected <- readRDS('meansdout.rda')
+  expect_equal(meansd(mtcars$mpg), expected[[1]])
+  expect_equal(meansd(x = mtcars$mpg, roundDig = 5, groupvar = mtcars$cyl, 
+                      range = TRUE, add_n = TRUE, .german = TRUE), 
+               expected[[2]])
+})
