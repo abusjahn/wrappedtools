644588e2daeb665c4ae3026c9acafe3917d71632
BilalsGituation <billyasser@hotmail.co.uk>
2022-08-24 14:55:13 +0200

    early tests for pull within Bilal repos

--- tests/testthat/test-FindVars.R
+++ tests/testthat/test-FindVars.R
@@ -4,7 +4,7 @@
 # out3 <- FindVars(varnames = c("^c", "g"))
 # saveRDS(list(out1=out1, out2=out2, out3=out3),file = 'tests/testthat/FindVars_out.rda')
 
-test_that("meansd() with defaults and options set", {
+test_that("FindVars() with defaults and options set", {
   expected <- readRDS('FindVars_out.rda')
   expect_equal(FindVars(varnames = c("^c", "g"), allnames = colnames(mtcars)), expected[[1]])
   expect_equal(FindVars(varnames = c("^c", "g"), allnames = colnames(mtcars), exclude = "r"), 
